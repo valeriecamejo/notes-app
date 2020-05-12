8effa225873c6bb02711490726ec4addb9696d09
valeriecamejo <valerie260492@gmail.com>
2020-05-12 07:11:12 -0400

    ADD: Se agrega conexión a la base de datos MongoDB usando el ORM mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const app = require('./app')
+require('./database')
 
 const main = async () => {
-  await app.listen(3000)
+  await app.listen(4000)
   console.log('Server on port 3000')
 }
 
