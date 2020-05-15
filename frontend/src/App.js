2477ba157a045a7fe70d5e1e9065ee5a136fca37
valeriecamejo <valerie260492@gmail.com>
2020-05-14 23:35:25 -0400

    ADD: Se agregó el cliente HTTP Axios para realizar las consultas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 import Navigation from './components/Navigation';
@@ -11,10 +12,12 @@ function App() {
   return (
     <Router>
       <Navigation />
-      <Route path="/" exact component={NoteList}/>
-      <Route path="/edit/:id" component={CreateNote}/>
-      <Route path="/create" component={CreateNote}/>
-      <Route path="/user" component={CreateUser}/>
+      <div className="container p-4">
+        <Route path="/" exact component={NoteList}/>
+        <Route path="/edit/:id" component={CreateNote}/>
+        <Route path="/create" component={CreateNote}/>
+        <Route path="/user" component={CreateUser}/>
+      </div>
     </Router>
   );
 }
